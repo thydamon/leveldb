63d52b23f074aed4844c06806b04729a1b15ea3d
unknown <tanghao27478@hs.handsome.com.cn>
2022-03-01 08:32:46 +0800

    arena

--- util/arena.cc
+++ util/arena.cc
@@ -14,6 +14,7 @@ Arena::Arena() : memory_usage_(0) {
   alloc_bytes_remaining_ = 0;
 }
 
+// 释放整个内存池所占内存
 Arena::~Arena() {
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
@@ -21,6 +22,10 @@ Arena::~Arena() {
 }
 
 char* Arena::AllocateFallback(size_t bytes) {
+  // 如果需求的内存大于内存块中剩余的内存，而且大于1K，则给这内存单独分配一块bytes大小的内存。
+  // 这样可以避免浪费过多的空间（因为如果bytes大于1K也从4K的内存块去取用，那么如果当前内存块中刚好剩余
+  // 1K，只能再新建一个4K的内存块，并且取用bytes。此时新建的内存块是当前内存块，后续操作都是基于当前内
+  // 存块的，那么原内存块中的1K空间就浪费了）
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
@@ -29,6 +34,9 @@ char* Arena::AllocateFallback(size_t bytes) {
   }
 
   // We waste the remaining space in the current block.
+  // 如果需求的内存大于内存块中剩余的内存，而且小于1K，则重新分配一个内存块，默认大小4K，
+  // 原内存块中剩余的内存浪费掉（这样虽然也会浪费，但是浪费的空间小于1K）。并在新内存块
+  // 中取用bytes大小的内存。
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
 
@@ -38,6 +46,15 @@ char* Arena::AllocateFallback(size_t bytes) {
   return result;
 }
 
+// 提供了字节对齐内存分配，一般情况是4字节或8个字节对齐分配，
+// 对齐内存的好处简单的说就是加速内存访问。
+// 首先获取一个指针的大小const int align = sizeof(void*)，
+// 很明显，在32位系统下是4 ,64位系统下是8 ，为了表述方便，我们假设是32位系统，即align ＝ 4,
+// 然后将我们使用的char * 指针地址转换为一个无符号整型(reinterpret_cast<uintptr_t>(result):
+// It is an unsigned int that is guaranteed to be the same size as a pointer.)，通过与操作来
+// 获取size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1);当前指针模4
+// 的值，有了这个值以后，我们就容易知道，还差 slop = align - current_mod多个字节，内存才是对齐的，
+// 所以有了result = alloc_ptr + slop。那么获取bytes大小的内存，实际上需要的大小为needed = bytes + slop。
 char* Arena::AllocateAligned(size_t bytes) 
 {
   // 32bit system pointer size is 4bytes,64bit system pointer size is 8bytes.
@@ -50,6 +67,7 @@ char* Arena::AllocateAligned(size_t bytes)
   // reinterpret_cast can convert from different type variable. es,char *_->long int
   // after convert with the same bits
   // alloc_ptr mod align-1
+  // 这里就判断出了当前指针地址和字节对齐整数倍所差的个数
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1); // alloc_ptr=4,align=4,current_mod == 0
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
   size_t needed = bytes + slop;
@@ -67,6 +85,7 @@ char* Arena::AllocateAligned(size_t bytes)
   return result;
 }
 
+// 分配新的内存块
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
   blocks_.push_back(result);
--- util/arena.h
+++ util/arena.h
@@ -15,19 +15,29 @@
 
 namespace leveldb {
 
+// ---------------------------------------------------
+// |  kBlockSize  |  kBlockSize  |  used  |  unused  |
+// ---------------------------------------------------
+//                                        |  alloc_bytes_remaining_ptr
+//                                  alloc_ptr_
 class Arena {
  public:
   Arena();
   ~Arena();
 
   // Return a pointer to a newly allocated memory block of "bytes" bytes.
+  // 分配bytes大小的内存块，返回指向该内存块的指针
   char* Allocate(size_t bytes);
 
   // Allocate memory with the normal alignment guarantees provided by malloc
+  // 基于malloc的字节对齐内存分配
   char* AllocateAligned(size_t bytes);
 
   // Returns an estimate of the total memory usage of data allocated
   // by the arena.
+  // 返回整个内存池使用内存的总大小（不精确）,这里只计算了已分配内存块的总大小和
+  // 存储各个内存块指针所用的空间。并未计算alloc_ptr_和alloc_bytes_remaining_
+  // 等数据成员的大小。
   size_t MemoryUsage() const {
     return reinterpret_cast<uintptr_t>(memory_usage_.NoBarrier_Load());
   }
@@ -37,13 +47,17 @@ class Arena {
   char* AllocateNewBlock(size_t block_bytes);
 
   // Allocation state
+  // 当前内存块(block)偏移量指针，也就是未使用内存的首地址
   char* alloc_ptr_;
+  // 表示当前内存块(block)中未使用的空间大小
   size_t alloc_bytes_remaining_;
 
   // Array of new[] allocated memory blocks
+  // 用来存储每一次向系统请求分配的内存块的指针
   std::vector<char*> blocks_;
 
   // Total memory usage of the arena.
+  // 迄今为止分配的内存块的总大小
   port::AtomicPointer memory_usage_;
 
   // No copying allowed
@@ -56,12 +70,15 @@ inline char* Arena::Allocate(size_t bytes) {
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
   assert(bytes > 0);
+  // 当前剩余字节数够分配，直接分配
   if (bytes <= alloc_bytes_remaining_) {
     char* result = alloc_ptr_;
     alloc_ptr_ += bytes;
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
+  // 因为alloc_bytes_remaining_初始为0，因此第一次调用Allocate实际上直接调用的是AllocateFallback
+  // 如果需求的内存大于内存块中剩余的内存，也会调用AllocateFallback
   return AllocateFallback(bytes);
 }
 
